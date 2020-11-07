cfbd3f804792e452c04ed88eb8b05e333619b281
Esteban Arroyo M <esteban.arroyo@gmail.com>
2020-11-07 01:45:55 -0300

    modificaciones varias

--- l10n_cl_mega/__manifest__.py
+++ l10n_cl_mega/__manifest__.py
@@ -2,13 +2,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 # Copyright (c) 2019 - Blanco Martín & Asociados. https://www.bmya.cl
 {
-    'name': 'Chile - Accounting Mega',
+    'name': 'Chile - Accounting MegaMedia',
     'version': "3.0",
     'description': """
 Chilean accounting chart and tax localization.
 Plan contable chileno e impuestos de acuerdo a disposiciones vigentes
     """,
-    'author': 'Blanco Martín & Asociados',
+    'author': 'Tierra Nube',
     'category': 'Accounting/Localizations/Account Charts',
     'depends': [
         'contacts',
@@ -28,16 +28,16 @@ Plan contable chileno e impuestos de acuerdo a disposiciones vigentes
         'data/account_tax_group_data.xml',
         'data/account_tax_tags_data.xml',
         'data/account_tax_data.xml',
-        'data/l10n_latam_identification_type_data.xml',
-        'data/l10n_latam.document.type.csv',
+        #'data/l10n_latam_identification_type_data.xml',
+        #'data/l10n_latam.document.type.csv',
         'data/menuitem_data.xml',
-        'data/product_data.xml',
-        'data/uom_data.xml',
+        #'data/product_data.xml',
+        #'data/uom_data.xml',
         'data/res.currency.csv',
         # 'data/res_currency_data.xml',
-        'data/res.bank.csv',
-        'data/res.country.csv',
-        'data/res_partner.xml',
+        #'data/res.bank.csv',
+        #'data/res.country.csv',
+        #'data/res_partner.xml',
         'data/account_fiscal_template.xml',
         'data/account_chart_template_data.xml',
     ],
